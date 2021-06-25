73c4751be579f80fcd77238a4534b9b551758a93
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-06-25 19:10:47 -0300

    push to server

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://fbsdev-uploads3.herokuapp.com"
+  baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
+
+//REACT_APP_API_URL =  config vars url app heroku url do server backend  https://fbsdev-uploads3.herokuapp.com
