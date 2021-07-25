0daf1f8211ce8d6d2715205a526c9b0a7badb8b8
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-25 20:24:55 -0300

    Tittle css edit

--- src/Title.js
+++ src/Title.js
@@ -4,8 +4,8 @@ import './Title.css'
 function Title() {
   return (
     <div className="Title">
-      <h1>FBS-DEV Upload de imagens</h1>
-            <div className="Title-Subtitle">upload de imagens de qualquer formatado até 8mb para o bucket s3 da aws </div>
+      <h1>Upload de imagens</h1>
+            <div className="Title-Subtitle">upload de qualquer formato até 8mb para o bucket s3 da aws </div>
     </div>
   )
 }
