97815605279996b27efaf7b69783c71e9648418c
FranciscoBSpadaro <francisco.spadaro@outlook.com>
2021-07-25 15:18:36 -0300

    index upload text

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -7,7 +7,7 @@ import { DropContainer, UploadMessage } from "./styles";
 export default class Upload extends Component {
   renderDragMessage = (isDragActive, isDragReject) => {
     if (!isDragActive) {
-      return <UploadMessage>Arraste arquivos aqui ou click...</UploadMessage>;
+      return <UploadMessage>Clique aqui ou arraste arquivos de imagem para upload</UploadMessage>;
     }
 
     if (isDragReject) {
